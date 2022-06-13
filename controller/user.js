d7e5eaebedef00e2fd11552eca650a6f1e835b6b
ankit <ankityadaav772@gmail.com>
2022-06-13 15:11:04 +0530

    validation cylcle update

--- controller/user.js
+++ controller/user.js
@@ -1,50 +1,50 @@
 const jwt = require('jsonwebtoken')
 const sendMail = require('../NodeMailer/sms');
-const { hashPassword, matchPassword } =require ('../bcrypt/bcrypt');
-const {UserCollection} = require('../db_connect/db_connect');
+const { hashPassword, matchPassword } = require('../bcrypt/bcrypt');
+const { UserCollection } = require('../db_connect/db_connect');
 
 
-exports.addUser= async (req, res) => {
-    const passHash =  await hashPassword(req.body.password);
+exports.addUser = async (req, res) => {
+    const passHash = await hashPassword(req.body.password);
     req.body.password = passHash;
 
     try {
-      const userDetail = UserCollection(req.body)
-      console.log(userDetail);
-      const result = await UserCollection.findOne({ email: userDetail.email })
-      if(result){
-          res.send({msg:"user exists"})
-      }
-      else
-     {
-         await userDetail.save((err, userDetail) => {
-          if (err) {
-              res.send({ err :err.message})
-          }
-          else {
-              res.status(200).send({ data: userDetail })
-          }
-      });}
-  }catch(err){
-      res.status(500).send(err);
-  }
-// const emailResult = await sendMail();
-//   console.log("00000",emailResult);
+        const userDetail = UserCollection(req.body)
+        console.log(userDetail);
+        const result = await UserCollection.findOne({ email: userDetail.email })
+        if (result) {
+            res.send({ msg: "user exists" })
+        }
+        else {
+            await userDetail.save((err, userDetail) => {
+                if (err) {
+                    res.send({ err: err.message })
+                }
+                else {
+                    console.log(userDetail);
+                    res.status(200).send({ data: userDetail })
+                }
+            });
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
+    // const emailResult = await sendMail();
+    //   console.log("00000",emailResult);
 
-  }
+}
 
-  exports.loginUser=async (req, res) => { 
+exports.loginUser = async (req, res) => {
     try {
-        const {emailphone,password} = req.body
-        const result = await UserCollection.findOne({$or:[{ email:emailphone },{ phone:emailphone }]})
-        const comparePassword =  matchPassword(password,result.password) ;
-        if (result && comparePassword) {  
-            const token = jwt.sign({ result }, 'ankit',{expiresIn:"2h"})
+        const { emailphone, password } = req.body
+        const result = await UserCollection.findOne({ $or: [{ email: emailphone }, { phone: emailphone }] })
+        const comparePassword = await matchPassword(password, result.password);
+        if (result && comparePassword) {
+            const token = jwt.sign({ result }, 'ankit', { expiresIn: "2h" })
             console.log(token);
             res.status(200).send({ token })
         }
-        else 
-        {
+        else {
             res.status(500).send({ msg: "user not found" })
         }
     }
@@ -53,25 +53,25 @@ exports.addUser= async (req, res) => {
     }
 }
 
-exports.getUsers= async (req,res)=>{
-    try{
-        const data = await  UserCollection.find({});
-        res.status(200).send({data})
+exports.getUsers = async (req, res) => {
+    try {
+        const data = await UserCollection.find({});
+        res.status(200).send({ data })
     }
-    catch(e){
-        res.status(404).send({msg:'error'})
+    catch (e) {
+        res.status(404).send({ msg: 'error' })
     }
 }
 
-exports.updateUser = async (req,res)=>{
-      try{
-          const {name,phone,email} = req.body 
-          const result = await  UserCollection.findOneAndUpdate({email},{$set:{name,phone,email}},{new: true})
-           result ? res.status(200).send({result}) : res.status(404).send({msg:"user not found"})
-          
-      }
-      catch(e){
-          console.log(e);
-      }
+exports.updateUser = async (req, res) => {
+    try {
+        const { name, phone, email } = req.body
+        const result = await UserCollection.findOneAndUpdate({ email }, { $set: { name, phone, email } }, { new: true })
+        result ? res.status(200).send({ result }) : res.status(404).send({ msg: "user not found" })
+
+    }
+    catch (e) {
+        console.log(e);
+    }
 
 }
